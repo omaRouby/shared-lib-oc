db025c8a9fc262024998062f9f04ddc114ea57e7
Omar Ahmed Rouby <164004472+omaRouby@users.noreply.github.com>
2024-06-06 00:29:46 +0300

    Create deployToOpenShift.groovy

---
+++ vars/deployToOpenShift.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/env groovy
+
+def call(String OPENSHIFT_CREDENTIALS_ID, String OPENSHIFT_PROJECT, String CLUSTER_URL) {
+
+    
+    // Login to OpenShift using the service account token
+    withCredentials([string(credentialsId: OPENSHIFT_CREDENTIALS_ID, variable: 'OC_TOKEN')]) {
+        sh "oc login --token=$OC_TOKEN --server=$CLUSTER_URL --insecure-skip-tls-verify"
+    }
+
+    // Apply the updated deployment.yaml to the OpenShift cluster
+    sh "oc apply -f . --namespace=${OPENSHIFT_OPROJECT}"
+}
