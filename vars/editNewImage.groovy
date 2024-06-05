f0ff9c5ee7a16cff1b219497084b14fca291d9e8
Omar Ahmed Rouby <164004472+omaRouby@users.noreply.github.com>
2024-06-06 01:01:40 +0300

    Update editNewImage.groovy

--- vars/editNewImage.groovy
+++ vars/editNewImage.groovy
@@ -1,8 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(String imageName) {
-    
     // Edit deployment.yaml with new Docker Hub image
-    sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yml"
-
+    sh "sed -i 's|image:.*|image: ${imageName}|g' deployment.yml"
 }
